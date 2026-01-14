92b7eefd7baa8e227c1e7cc4f7a17db12255e4d1
ManishKumarLal1 <106573768+ManishKumarLal1@users.noreply.github.com>
2026-01-15 01:12:57 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 
 /* ---------- MIDDLEWARES ---------- */
 app.use(express.json());
-app.use(cors{origin:ENV.CLIENT_URL});
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
 /* ---------- API ROUTES ---------- */
 app.get("/health", (req, res) => {
