81b0aef8facb113215e42e035824ab87b289e277
ManishKumarLal1 <106573768+ManishKumarLal1@users.noreply.github.com>
2026-01-22 15:58:11 +0530

    stream update

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,29 @@
+import {StreamChat} from "stream";
+import { ENV } from "./env.js"
+
+const apiKey= ENV.STREAM_API_KEY;
+const apiSecret= ENV.STREAM_API_SECRET;
+
+if(!apiKey||!apiSecret){
+    console.error("API KEY or API SECRET is missing");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser = async(userData)=>{
+    try {
+        await chatClient.upsertUser(userData);
+        console.log("User upserted Successfully:", userData)
+    } catch (error) {
+        console.error("Error in upserting Stream User:", error);
+    }
+}
+
+export const deleteStreamUser = async(userId)=>{
+    try {
+        await chatClient.deleteUser(userId);
+       console.log("User deleted Successfully:", userId)
+    } catch (error) {
+        console.error("Error in deleting Stream User:", error);
+    }
+}
